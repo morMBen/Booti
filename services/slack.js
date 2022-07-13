debdbffc4965c27533ff5d9999a8b22bd19ce050
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-13 09:34:51 +0300

    fix

--- services/slack.js
+++ services/slack.js
@@ -3,9 +3,7 @@ const { App, logLevel } = require('@slack/bolt');
 const express = require('express');
 // const Message = require('../models/message.js');
 
-const { User } = require('../models/user.js');
-const User2 = require('../models/user.js');
-// const Reaction = require('../models/reaction.js');
+const User = require('../models/user.js');
 
 const app = new App({
   token: process.env.TOKEN,
@@ -13,29 +11,26 @@ const app = new App({
 });
 
 app.message('', async ({ message, say }) => {
-  console.log(User);
-  console.log(User2);
-  console.log(User2.module.find);
-  console.log(User2.find);
+  // console.log(User);\
   // const user = await new User({ slack_display_name: 'Bob', slack_user_id: 'ID' });
   // user.save();
 
-  // let user = await User.find({ slack_user_id: message.user });
-  // console.log(User);
+  let user = await User.find({ slack_user_id: message.user });
+  console.log(User);
 
-  // if (!user) {
-  //   const userData = await app.client.users.info({ token: process.env.TOKEN, user: message.user });
-  //   const {
-  //     user: {
-  //       profile: { display_name },
-  //     },
-  //   } = userData;
+  if (!user) {
+    const userData = await app.client.users.info({ token: process.env.TOKEN, user: message.user });
+    const {
+      user: {
+        profile: { display_name },
+      },
+    } = userData;
 
-  // user = await new User({ slack_display_name: display_name, slack_user_id: message.user });
-  // user.save();
-  // }
+    user = await new User({ slack_display_name: display_name, slack_user_id: message.user });
+    user.save();
+  }
 
-  // say(`thanks <@${user.slack_display_name}>`);
+  say(`thanks <@${user.slack_display_name}>`);
   try {
   } catch (e) {
     console.log(e);
