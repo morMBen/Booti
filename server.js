9b9dfb0473e40b910466d6fbc0486a363fadc932
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-13 15:20:14 +0300

    ☺

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send(<h1>Hello</h1>);
+});
+
 app.listen(8000, () => {
   console.log('listen to port -> ', 8000);
 });
