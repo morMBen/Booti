27a957802324878d91e8031800fe3a63121af8d6
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-13 15:17:30 +0300

    ☺

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 require('./dbConnect.js');
 require('./services/slack.js');
-require('./express.js');
+const express = require('express');
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log('listen to port ->', process.env.PORT);
+});
