5bdca7f0f1627d6614067ff80de0ff8196ed596f
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-13 15:20:52 +0300

    ☺

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(<h1>Hello</h1>);
+  res.send('<h1>Hello</h1>');
 });
 
 app.listen(8000, () => {
