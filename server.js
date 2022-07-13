6f1713ef4a53913d8540f8871e87b53eaa543a87
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-13 15:18:19 +0300

    ☺

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const express = require('express');
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log('listen to port ->', process.env.PORT);
+app.listen(5000, () => {
+  console.log('listen to port -> ', 5000);
 });
