0ba74e98ad5163ca916b297fc1bbf51502a18ed7
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-13 14:39:03 +0300

    fix

--- services/slack.js
+++ services/slack.js
@@ -51,7 +51,7 @@ app.event('reaction_added', async ({ event, client }) => {
       user: 'U03P28WA337',
     });
   } catch (e) {
-    console.log(e.message);
+    console.dir(e.message);
   }
   // try {
   //   const sender = await User.setUser(event.user, app);
