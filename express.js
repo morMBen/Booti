22e657eaddbd18eb01e3527f35ca949e9bd38c48
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-18 15:26:44 +0300

    express init

--- express.js
+++ express.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello</h1>');
-});
+app.use(express.json());
+app.use(express.urlencoded());
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello</h1>');
+// });
 
 app.listen(8000, () => {
   console.log('listen to port -> ', 8000);
