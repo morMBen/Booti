4200ef23a5ab8571edff0c94ec88e7e0c2f75d12
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-31 13:09:48 +0300

    ☺

--- models/user.js
+++ models/user.js
@@ -32,16 +32,16 @@ userSchema.statics.setUser = async (id, app) => {
   }
 };
 
-userSchema.virtual('reactions').get(async function () {
-  return await Reaction.find({
-    receiver: this._id,
-    type: 'white_check_mark',
-  });
-  // .where('this.sender===this.parent_user')
-  // .where('sender')
-  // .ne(this._id)
-  // .count();
-});
+// userSchema.virtual('reactions').get(async function () {
+//   return await Reaction.find({
+//     receiver: this._id,
+//     type: 'white_check_mark',
+//   });
+// .where('this.sender===this.parent_user')
+// .where('sender')
+// .ne(this._id)
+// .count();
+// });
 
 // userSchema.virtual('right_answers').get(async function () {
 //   return await Reaction.find({
