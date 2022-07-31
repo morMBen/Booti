92aa9a9e95119dff2ec1a615ca305c0af5f6c4ee
Mor Mordechai Ben <mor6686@gmail.com>
2022-07-31 12:18:39 +0300

    ☺

--- routers/index.js
+++ routers/index.js
@@ -6,10 +6,10 @@ const route = express.Router();
 
 route.get('/users', async (req, res) => {
   try {
-    const user = User.findById(req.body.id);
-    console.log('/users  right_answers', user.right_answers);
-    console.log('/users  reactions', user.reactions);
-    res.send(user.right_answers);
+    const user = await User.findById(req.body.id);
+    console.log('/users  right_answers', await user.right_answers);
+    console.log('/users  reactions', await user.reactions);
+    res.send(await user.right_answers);
   } catch (e) {
     console.log(e.message);
     res.send(e.message);
