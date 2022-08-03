92fd92553b958bb31f74c585bde964e01746727d
Mor Mordechai Ben <mor6686@gmail.com>
2022-08-03 11:46:02 +0300

    ☺

--- express.js
+++ express.js
@@ -19,7 +19,7 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('process1', process.env.NODE_ENV);
+  console.log('process1', process);
   console.log('listen to port -> ', port);
 });
 
