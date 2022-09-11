1146a8e1e7db7066e71ea0eac27c0ddb4e04e645
Mor Mordechai Ben <mor6686@gmail.com>
2022-09-11 15:11:16 +0300

    ☺

--- services/slack.js
+++ services/slack.js
@@ -48,7 +48,8 @@ function observeBootcamp(bootcampObj) {
   app.event('message', async ({ event, client }) => {
     console.log('my message delete → ', event);
     if (event.subtype && event.subtype === 'message_deleted') {
-      console.log('message deleted → ', event.previous_message.ts);
+      const mes = Message.findOne({ slack_message_id: event.previous_message.ts });
+      console.log('message deleted → ', mes);
     }
   });
   app.event('reaction_removed', async ({ event, client }) => {
